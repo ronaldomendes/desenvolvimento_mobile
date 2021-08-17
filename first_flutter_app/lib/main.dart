98212b888dd94057f102c192c58feb10105245e1
ronaldomendes <mendes-ronaldo@live.com>
2021-08-17 20:11:15 -0300

    first_flutter_app second update

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Flutter Demo",
+        title: "First Flutter App",
         home: Scaffold(
-          body: Center(
-            child: Text("Hello Flooter"),
-          ),
+          backgroundColor: Color.fromRGBO(255, 255, 200, 0.5),
+          appBar: AppBar(title: Text("Flutter Demo App"), centerTitle: true),
+          body: Center(child: Text("Hello Flooter! Hahahah")),
         ));
   }
 }
