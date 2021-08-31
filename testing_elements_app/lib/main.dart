054f9398109b4a44a5974b3630c42a7b43fbb04b
ronaldomendes <mendes-ronaldo@live.com>
2021-08-31 20:38:22 -0300

    Simple layout in column

--- testing_elements_app/lib/main.dart
+++ testing_elements_app/lib/main.dart
@@ -10,10 +10,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Dado',
-        home: Container(
-          color: Colors.white10,
-          child: Image.network(
-              'https://th.bing.com/th/id/OIP.CyJGDESPuyApOOXzHdAwAAAAAA?pid=ImgDet&rs=1'),
+        home: Column(
+          children: <Widget>[
+            Text('New Application'),
+            Container(
+              color: Colors.red[400],
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                      'https://th.bing.com/th/id/OIP.hKcqmg81dDFu_wjYIl33IAAAAA?pid=ImgDet&w=218&h=150&rs=1'),
+                  ElevatedButton(
+                      onPressed: () {
+                        print('Button was pressed');
+                      },
+                      child: Text('Button'))
+                ],
+              ),
+            ),
+          ],
         ));
   }
 }
